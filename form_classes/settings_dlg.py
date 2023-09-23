b2c02673b803f8a340bf699f8bdf1050fb46a2b6
schwemmdx <sven.prigann@web.de>
2023-09-23 20:10:47 +0200

    added settings logic

--- form_classes/settings_dlg.py
+++ form_classes/settings_dlg.py
@@ -9,4 +9,49 @@ class SettingsDialog(QDialog):
         self.ui = Ui_SettingsDialog()
         self.ui.setupUi(self)
         self.hide()
+
+
+        ##later use settings-file for loading previous data
+        self.data = {
+            'tcp':{
+                'ip': "192.168.0.57",
+                'port': 5025,
+                'timeout': 1000
+            },
+            'misc': {},
+            'graph':{}
+        }
+
+        self.__setData()
+
+        self.ui.buttonBox.accepted.connect(self.on_ok_clicked)
+        self.ui.buttonBox.rejected.connect(self.on_reject_cklicked)
+
+
+    def __setData(self):
+        self.ui.timeoutVal.setValue(self.data['tcp']['timeout'])
+        self.ui.ipAddr.setText(self.data['tcp']['ip'])
+        self.ui.portVal.setValue(self.data['tcp']['port'])
         
+    def __getData(self):
+         
+        self.data['tcp'] = {
+            'ip': self.ui.ipAddr.text(),
+            'port' : self.ui.portVal.value(),
+            'timeout': self.ui.timeoutVal.value()
+        }
+
+        self.data['misc'] = {}
+        self.data['graph'] = {}
+        
+
+    def getTCPSettings(self):
+        return self.data['tcp']   
+
+    def on_reject_cklicked(self):
+        self.__setData()
+        self.hide()     
+    
+    def on_ok_clicked(self):
+        self.__getData()
+        self.hide()
