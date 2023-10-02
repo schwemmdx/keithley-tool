6275bc701d0a755e8b77296314265291cc15a1ff
schwemmdx <sven.prigann@web.de>
2023-10-02 23:20:44 +0200

    added logic for sweep run method

--- form_classes/sweep_widget.py
+++ form_classes/sweep_widget.py
@@ -2,6 +2,7 @@
 from typing import Optional
 import PySide6.QtCore
 from PySide6.QtWidgets import QWidget
+from modules.keithley_lib import K2636
 
 from pyforms.ui_sweep_widget import Ui_SweepWidget
 
@@ -12,7 +13,7 @@ import numpy as np
 import json
 
 class SweepWidget(QWidget):
-    def __init__(self, parent: QWidget | None ,instr=None) -> None:
+    def __init__(self, parent: QWidget | None ,instr: K2636) -> None:
         super().__init__(parent)#
         self.instr = instr
 
@@ -47,6 +48,26 @@ class SweepWidget(QWidget):
         sweepCfg = self.getSweepValues()
         with open("./settings/sweep_settings.json",'w') as pOut:
             json.dump(sweepCfg,pOut,indent=4)
+
+            #check if we only perform a sweep on one smu 
+            if self.ui.smuA_grp.isEnabled() and self.ui.smub_grp.isEnabled():
+                #perform dual smu sweep
+                self.sweepDualSMU(self.getSweepValues())
+            elif self.ui.smuA_grp.isEnabled():
+                #perfom sweep on smua
+                self.sweepSMU('smua',self.getSweepValues()['smua'])
+            else:
+                self.sweepSMU('smub',self.getSweepValues()['smub'])
+
+        
+    def sweepSMU(self,smuName,sweepCfg):
+        sweepVals = np.arange(sweepCfg['start'],sweepCfg['stop'],sweepCfg['step'])
+        self.instr
+        
+
+    def sweepDualSMU(self,sweepCfg):
+        pass
+
 
 
 
